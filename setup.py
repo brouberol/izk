798aa160cc8f2eca8b52f8489a42b7ee62cf890f
Balthazar Rouberol <balthazar.rouberol@datadoghq.com>
2018-01-07 22:27:07 +0100

    Fix apache license classifier

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: Apache 2.0 License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
